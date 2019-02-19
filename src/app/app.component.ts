0feda31023d7ef345ffe85501ef7955f1a5895a0
Subrat <subrat@Subrats-MacBook.local>
2019-02-19 11:23:06 +0530

    socket changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
 
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,10 +12,24 @@ import { Chart } from 'chart.js';
 })
 export class AppComponent implements OnInit {
   title = 'dashboard';
-  chart = [];
+  chart;
   chart2 = [];
+  pie: any;
+  doughnut: any;
+
+  data1 = [];
 
   ngOnInit() {
+
+    socket.on('data1', (res) => {
+      this.updateChartData(this.chart, res, 0);
+      this.updateChartData(this.doughnut,res.slice(0,5), 0);
+    })
+
+    socket.on('data2', (res) => {
+      this.updateChartData(this.chart, res, 1);
+    })
+
     this.chart = new Chart('bar', {
       type: 'bar',
       options: {
@@ -27,21 +45,21 @@ export class AppComponent implements OnInit {
           {
             type: 'bar',
             label: 'My First dataset',
-            data: [443, 256, 165, 10, 156, 265, 356, 543],
+            data: [243, 156, 365, 30, 156, 265, 356, 543],
             backgroundColor: 'rgba(255,0,255,0.4)',
             borderColor: 'rgba(255,0,255,0.4)',
             fill: false,
           },
-          {
-            type: 'line',
-            label: 'Dataset 2',
-            backgroundColor: 'rgba(0,0,255,0.4)',
-            borderColor: 'rgba(0,0,255,0.4)',
-            data: [
-              443, 256, 165, 100, 56, 65, 35, 543
-            ],
-            fill: true,
-          },
+          // {
+          //   type: 'line',
+          //   label: 'Dataset 2',
+          //   backgroundColor: 'rgba(0,0,255,0.4)',
+          //   borderColor: 'rgba(0,0,255,0.4)',
+          //   data: [
+          //     443, 256, 165, 100, 56, 65, 35, 543
+          //   ],
+          //   fill: true,
+          // },
           {
             type: 'bar',
             label: 'My Second dataset',
@@ -134,7 +152,7 @@ export class AppComponent implements OnInit {
       }
     })
 
-    new Chart('doughnut',{
+    this.doughnut =  new Chart('doughnut',{
       type: 'doughnut',
       options: {
         responsive: true,
@@ -158,7 +176,7 @@ export class AppComponent implements OnInit {
 			}
     })
 
-    new Chart('pie',{
+    this.pie = new Chart('pie',{
       type: 'pie',
       options: {
         responsive: true,
@@ -183,5 +201,26 @@ export class AppComponent implements OnInit {
     })
 
   }
+
+  addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+removeData(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+}
+
+updateChartData(chart, data, dataSetIndex){
+  chart.data.datasets[dataSetIndex].data = data;
+  chart.update();
+}
 
 }
